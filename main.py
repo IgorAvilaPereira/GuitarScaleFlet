19b023142f9c140f4c9a5e4ad86c6b7c04ee875c
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-11-21 11:41:21 -0300

    demo

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import flet as ft
 from datetime import datetime
+import sqlite3
+
 
 # dedo, corLetra, corFundo
 violao = [
@@ -169,6 +171,7 @@ def main(page: ft.Page):
         reiniciar()
         open_dlg(e)     
 
+    # print(page.client_storage.get_keys("key-prefix."))
     page.add(
         ft.DataTable(
             columns=[
@@ -239,7 +242,7 @@ def main(page: ft.Page):
 
     page.add(ft.ElevatedButton(text="Salvar", on_click=salvar))
     # recuperar chave salva
-    print(page.client_storage.get("20_11_2023_22_44_32"))
+    print(page.client_storage.get("*"))
 
 # https://flet.dev/docs/guides/python/testing-on-android/
 # https://flet.dev/docs/cli/run/
@@ -252,8 +255,218 @@ def main(page: ft.Page):
 # python main.py
 
 # destop
-ft.app(target=main)
+# ft.app(target=main)
 
 # web
-# ft.app(target=main, view=ft.AppView.WEB_BROWSER)
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
 
+# @app.route("/tela_adicionar_tags/<int:id>", methods=['GET'])
+# def tela_adicionar_tags(id):
+#     conn = sqlite3.connect("database.db")
+#     cur = conn.cursor()
+#     cur.execute("SELECT * FROM questoes where questoes.id = ?", [id])
+#     questao = cur.fetchone()
+
+#     cur.execute("SELECT * FROM tags")    
+#     vetTag = cur.fetchall()
+
+#     cur.execute("SELECT tag_id FROM questoes inner join questoes_tags on (questoes.id = questao_id) where questoes.id = ?", [id])
+#     vetQuestaoTag = []
+#     for i in cur.fetchall():
+#         vetQuestaoTag.append(i[0])
+#     cur.close()
+#     conn.close()
+#     return render_template('tela_adicionar_tags.html', vetTag=vetTag, questao=questao, vetQuestaoTag=vetQuestaoTag)    
+    
+
+# @app.route("/tela_alterar_questao/<int:id>", methods=['GET'])
+# def tela_alterar_questao(id):
+#     conn = sqlite3.connect("database.db")
+#     cur = conn.cursor()
+#     cur.execute("SELECT * FROM questoes where id = ?", [id])
+#     # conn.commit()   
+#     questao = cur.fetchone()
+#     cur.close()
+#     conn.close()
+#     # return redirect(url_for('index'))
+#     return render_template('tela_alterar_questao.html', questao=questao)
+
+# @app.route("/tela_alterar_tag/<int:id>", methods=['GET'])
+# def tela_alterar_tag(id):
+#     conn = sqlite3.connect("database.db")
+#     cur = conn.cursor()
+#     cur.execute("SELECT * FROM tags where id = ?", [id])
+#     # conn.commit()   
+#     tag = cur.fetchone()
+#     cur.close()
+#     conn.close()
+#     # return redirect(url_for('index'))
+#     return render_template('tela_alterar_tag.html', tag=tag)
+
+# @app.route("/remover_tag/<int:id>", methods=['GET'])
+# def remover_tag(id):
+#     conn = sqlite3.connect("database.db")
+#     cur = conn.cursor()
+#     cur.execute("DELETE FROM tags where id = ?", [id])
+#     conn.commit()   
+#     cur.close()
+#     conn.close()
+#     return redirect(url_for('index'))
+
+# @app.route("/adicionar_tags", methods=['POST'])
+# def adicionar_tags():
+#     id = int(request.form.get("id"))
+    
+#     conn = sqlite3.connect("database.db")
+#     cur = conn.cursor()
+
+#     cur.execute("DELETE FROM questoes_tags WHERE questao_id = ?;", [id])
+#     conn.commit()   
+
+#     for tag_id in request.form.getlist('tags'):    
+#         cur.execute("INSERT INTO questoes_tags (questao_id, tag_id) VALUES(?,?);", [id, tag_id])    
+#         conn.commit()   
+
+#     cur.close()
+#     conn.close()
+#     return redirect(url_for('index'))
+
+# @app.route("/alterar_tag", methods=['POST'])
+# def alterar_tag():
+#     conn = sqlite3.connect("database.db")
+#     cur = conn.cursor()
+#     tag = request.form.get("tag")   
+#     id = int(request.form.get("id"))
+#     cur.execute("UPDATE tags SET tag = ? where id = ?", [tag, id])
+#     conn.commit()   
+#     cur.close()
+#     conn.close()
+#     return redirect(url_for('index'))
+
+# @app.route("/alterar_questao", methods=['POST'])
+# def alterar_questao():
+#     conn = sqlite3.connect("database.db")
+#     cur = conn.cursor()
+#     questao = request.form.get("questao")   
+#     id = int(request.form.get("id"))
+#     cur.execute("UPDATE questoes SET questao = ? where id = ?", [questao, id])
+#     conn.commit()   
+#     cur.close()
+#     conn.close()
+#     return redirect(url_for('index'))
+
+# @app.route("/remover_questao/<int:id>", methods=['GET'])
+# def remover_questao(id):
+#     conn = sqlite3.connect("database.db")
+#     cur = conn.cursor()
+#     cur.execute("DELETE FROM questoes where id = ?", [id])
+#     conn.commit()   
+#     cur.close()
+#     conn.close()
+#     return redirect(url_for('listar_questao'))
+
+# @app.route("/adicionar_tag", methods=['POST'])
+# def adicionar_tag():
+#     tag = request.form.get("tag")        
+#     conn = sqlite3.connect("database.db")
+#     cur = conn.cursor()
+#     cur.execute("INSERT INTO tags (tag) VALUES (?)", [tag])
+#     conn.commit()   
+#     cur.close()
+#     conn.close()
+#     return redirect(url_for('index'))
+
+# @app.route("/adicionar_questao", methods=['POST'])
+# def adicionar_questao():
+#     questao = request.form.get("questao")        
+#     conn = sqlite3.connect("database.db")
+#     cur = conn.cursor()
+#     cur.execute("INSERT INTO questoes (questao) VALUES (?)", [questao])
+#     conn.commit()   
+#     cur.close()
+#     conn.close()
+#     return redirect(url_for('index'))
+
+# @app.route("/gerar", methods=['POST'])
+# def gerar():
+#     nro_questao = 1
+#     nro_questao = int(request.form.get("nro_questao"))   
+    
+#     vetTag = list(map(int, request.form.getlist('tags')))       
+    
+#     conn = sqlite3.connect("database.db")
+#     cur = conn.cursor()
+    
+#     if (vetTag is not None):
+#         if (len(vetTag) > 0):
+#             tags = "("
+#             for t in vetTag:
+#                 tags = tags + str(t) + ","
+#             tags = tags + ")"
+#             tags = tags.replace(",)", ")")         
+
+#             cur.execute("SELECT * FROM questoes inner join questoes_tags where tag_id in "+tags+" ORDER BY random() limit "+str(nro_questao))
+#         else:
+#             # print("aqui2")
+#             cur.execute("SELECT * FROM questoes ORDER BY random() limit "+str(nro_questao))
+#     else:
+#         # print("aqui")
+#         cur.execute("SELECT * FROM questoes ORDER BY random() limit "+str(nro_questao))
+    
+#     vetQuestao = cur.fetchall()
+    
+#     cur.close()
+#     conn.close()
+#     return render_template('prova.html', vetQuestao=vetQuestao)
+
+# @app.route("/listar_questao/", defaults={ 'page': 1 })
+# @app.route("/listar_questao/<int:page>")
+# def listar_questao(page):
+#     records = 10
+#     start_from = (page-1)*records
+#     conn = sqlite3.connect("database.db")
+#     cur = conn.cursor()        
+#     cur.execute("select count(*) as total from questoes")    
+#     total_records = cur.fetchone()[0]
+#     total_pages = math.ceil(total_records/records)    
+#     cur.execute("select * FROM questoes order by id desc limit "+str(records)+" offset "+str(start_from))
+#     vet = cur.fetchall()
+#     cur.close()
+#     conn.close()
+#     html_paginacao = ""
+#     i = 1
+#     while (i <= total_pages):        
+#         html_paginacao = html_paginacao + "<a href='"+str(i)+"'>"+str(i)+"</a>&nbsp;&nbsp;"; 
+#         i = i + 1     
+#     return render_template('listar_questao.html', vet=vet, html_paginacao=html_paginacao)
+
+# @app.route("/")
+# def index():
+#     conn = sqlite3.connect("database.db")
+#     cur = conn.cursor()
+#     cur.execute("SELECT * FROM tags")
+#     vetTag = cur.fetchall()
+#     cur.close()
+#     conn.close()
+#     return render_template('index.html', vetTag=vetTag)
+
+# @app.route("/importar")
+# def importar():
+#     # conSqlite3 = sqlite3.connect("database.db")
+#     # curSqlite3 = conSqlite3.cursor()
+#     # curSqlite3.execute("INSERT INTO questoes (questao) VALUES(?)", [questao[1]])
+#     # conSqlite3.commit()     
+    
+#     conn = psycopg2.connect("host=localhost dbname=docente user=postgres password=postgres port=5432")
+#     cur = conn.cursor()
+#     cur.execute("SELECT * FROM avaliacoes where descricao is not null;")
+#     vetQuestao = cur.fetchall()
+#     for questao in vetQuestao:   
+#         # print(questao)            
+#         conSqlite3 = sqlite3.connect("database.db")
+#         curSqlite3 = conSqlite3.cursor()
+#         curSqlite3.execute("INSERT INTO questoes (questao) VALUES(?)", [questao[2]])
+#         conSqlite3.commit()    
+#     cur.close()
+#     conn.close()
+#     return "ok"
