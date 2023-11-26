6d0f3c9de7d75e46a75e446f66bb6f9a3c1d2a96
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-11-26 17:26:46 -0300

    ok

--- main.py
+++ main.py
@@ -450,62 +450,12 @@ def main(page: ft.Page):
             now = datetime.now()
             dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")   
             txt_name.value = txt_name.value+"_"+dt_string
-            # print(violao)
             salvar(e)     
-            # conn = sqlite3.connect("database.db")
-            # cur = conn.cursor()
-            # sql = "DELETE FROM notas where shape_id = ?"
-            # cur.execute(sql, [id]) 
-            # conn.commit()           
-            # i = 0  
-            # while (i < NRO_BOTOES):
-            #     botao = violao[i]
-            #     if (botao[0] == "1" or botao[0] == "2" or botao[0] == "3" or botao[0] == "4"):                                
-            #         sql = "INSERT INTO notas (botao, dedo, dominante, shape_id) values (?, ?, ?, ?);";                
-            #         cur.execute(sql, [str(i+2), botao[0], True if botao[2] == ft.colors.RED else False, id])         
-            #         conn.commit()                     
-            #     i = i + 1
-            # cur.close()
-            # conn.close()
-            open_dlg_duplicar_acerto(e)  
-            # open_dlg_editar(e)  
+            open_dlg_duplicar_acerto(e)              
         except:
-            open_dlg_duplicar_erro(e)  
-
-    # row = ft.Row(spacing=0, con)
-    # page.add(
-
-    # items = []
-    # for i in range(1, 1 + 1):
-    #     items.append(
-    #         ft.Container(
-    #             content=dataTable,
-    #             alignment=ft.alignment.top_left,
-    #             # width=50,
-    #             # height=50,
-    #             # bgcolor=ft.colors.AMBER,
-    #             # border_radius=ft.border_radius.all(5),
-    #         )
-    #     )
-    #     items.append(
-    #         ft.Container(
-    #             content=lv,
-    #             alignment=ft.alignment.center,
-    #             # width=50,
-    #             # height=50,
-    #             # bgcolor=ft.colors.AMBER,  
-    #             # border_radius=ft.border_radius.all(5),
-    #         )
-    #     )
-    
-    
-    # page.add(dataTable)  
-    # page.add(lv)  
-    # 
-      
+            open_dlg_duplicar_erro(e)       
 
     txt_name = ft.TextField(label="Nome do shape:")
-
 
     def items(count):
         items = []
@@ -513,86 +463,53 @@ def main(page: ft.Page):
             items.append(
                 ft.Container(
                     content=ft.ElevatedButton(text="Salvar", on_click=salvar),
-                    alignment=ft.alignment.center,
-                    # width=50,
-                    # height=50,
-                    # bgcolor=ft.colors.AMBER,
-                    # border_radius=ft.border_radius.all(5),
+                    alignment=ft.alignment.center
                 )
             )
             items.append(
                 ft.Container(
                     content=ft.ElevatedButton(text="Limpar", on_click=limpar),
-                    alignment=ft.alignment.center,
-                    # width=50,
-                    # height=50,
-                    # bgcolor=ft.colors.AMBER,
-                    # border_radius=ft.border_radius.all(5),
+                    alignment=ft.alignment.center
                 )
             )
             items.append(
                 ft.Container(
                     content=ft.ElevatedButton(text="Restaurar", on_click=restaurar),
-                    alignment=ft.alignment.center,
-                    # width=50,
-                    # height=50,
-                    # bgcolor=ft.colors.AMBER,
-                    # border_radius=ft.border_radius.all(5),
+                    alignment=ft.alignment.center
                 )
             )
             items.append(
                 ft.Container(
                     content=ft.ElevatedButton(text="Deletar", on_click=deletar),
-                    alignment=ft.alignment.center,
-                    # width=50,
-                    # height=50,
-                    # bgcolor=ft.colors.AMBER,
-                    # border_radius=ft.border_radius.all(5),
+                    alignment=ft.alignment.center
                 )
             )
 
             items.append(
                 ft.Container(
                     content=ft.ElevatedButton(text="Editar", on_click=editar),
-                    alignment=ft.alignment.center,
-                    # width=50,
-                    # height=50,
-                    # bgcolor=ft.colors.AMBER,
-                    # border_radius=ft.border_radius.all(5),
+                    alignment=ft.alignment.center
                 )
             )
 
             items.append(
                 ft.Container(
                     content=ft.ElevatedButton(text="Duplicar", on_click=duplicar),
-                    alignment=ft.alignment.center,
-                    # width=50,
-                    # height=50,
-                    # bgcolor=ft.colors.AMBER,
-                    # border_radius=ft.border_radius.all(5),
+                    alignment=ft.alignment.center
                 )
             )
-
 
             items.append(
                 ft.Container(
                     content=txt_name,
-                    alignment=ft.alignment.center,
-                    # width=50,
-                    # height=50,
-                    # bgcolor=ft.colors.AMBER,
-                    # border_radius=ft.border_radius.all(5),
+                    alignment=ft.alignment.center
                 )
-            )
-                 
+            )              
             
         return items
 
-
     row = ft.Row(spacing=0, controls=items(1))
     page.add(ft.Column(), row)
-
-
     page.add(
         ft.Row(
             [
@@ -600,273 +517,16 @@ def main(page: ft.Page):
                     content=dataTable,
                     margin=10,
                     padding=10,
-                    alignment=ft.alignment.center,
-                    # bgcolor=ft.colors.AMBER,
-                    # width=150,
-                    # height=150,
-                    # border_radius=10,
+                    alignment=ft.alignment.center
                 ),
                 ft.Container(
                     content=lv,
-                    # margin=10,
-                    # padding=10,
                     alignment=ft.alignment.center,
-                    # bgcolor=ft.colors.GREEN_200,
                     width=300,
-                    height=300,
-                    # border_radius=10,
-                    # on_click=lambda e: print("Clickable without Ink clicked!"),
+                    height=300
                 )
             ]))
-        
-    # row = ft.Row(spacing=0, controls=items)
-    # page.add(ft.Column(), row)    
-
-
-    lista_shapes()    
-
   
-
-    # page.add(ft.ElevatedButton(text="Salvar", on_click=salvar))
-    # page.add(ft.ElevatedButton(text="Limpar", on_click=limpar))    
-    # page.add(ft.ElevatedButton(text="Restaurar", on_click=restaurar))
-    # page.add(ft.ElevatedButton(text="Deletar", on_click=deletar))
-    # page.add(ft.ElevatedButton(text="Editar", on_click=editar))
-    # page.add(ft.ElevatedButton(text="Duplicar", on_click=duplicar))  
-    # recuperar chave salva
-    # print(page.client_storage.get("*"))
-
-# https://flet.dev/docs/guides/python/packaging-desktop-app/#customizing-windows-executable-metadata
-# Linux
-# flet pack main.py --add-data "assets:assets"
-# Windows
-# flet pack main.py --add-data "assets;assets"
-
-# https://flet.dev/docs/guides/python/testing-on-android/
-# https://flet.dev/docs/cli/run/
-
-# para rodar como app
-#  pip install flet --upgrade
-# flet run main.py --android
-
-# para rodar como desktop
-# python main.py
-
+    lista_shapes()
 # destop
 ft.app(target=main, assets_dir="assets")
-
-# web
-# autoreload
-# flet run main.py --web
-# ft.app(target=main, view=ft.AppView.WEB_BROWSER)
-
-# @app.route("/tela_adicionar_tags/<int:id>", methods=['GET'])
-# def tela_adicionar_tags(id):
-#     conn = sqlite3.connect("database.db")
-#     cur = conn.cursor()
-#     cur.execute("SELECT * FROM questoes where questoes.id = ?", [id])
-#     questao = cur.fetchone()
-
-#     cur.execute("SELECT * FROM tags")    
-#     vetTag = cur.fetchall()
-
-#     cur.execute("SELECT tag_id FROM questoes inner join questoes_tags on (questoes.id = questao_id) where questoes.id = ?", [id])
-#     vetQuestaoTag = []
-#     for i in cur.fetchall():
-#         vetQuestaoTag.append(i[0])
-#     cur.close()
-#     conn.close()
-#     return render_template('tela_adicionar_tags.html', vetTag=vetTag, questao=questao, vetQuestaoTag=vetQuestaoTag)    
-    
-
-# @app.route("/tela_alterar_questao/<int:id>", methods=['GET'])
-# def tela_alterar_questao(id):
-#     conn = sqlite3.connect("database.db")
-#     cur = conn.cursor()
-#     cur.execute("SELECT * FROM questoes where id = ?", [id])
-#     # conn.commit()   
-#     questao = cur.fetchone()
-#     cur.close()
-#     conn.close()
-#     # return redirect(url_for('index'))
-#     return render_template('tela_alterar_questao.html', questao=questao)
-
-# @app.route("/tela_alterar_tag/<int:id>", methods=['GET'])
-# def tela_alterar_tag(id):
-#     conn = sqlite3.connect("database.db")
-#     cur = conn.cursor()
-#     cur.execute("SELECT * FROM tags where id = ?", [id])
-#     # conn.commit()   
-#     tag = cur.fetchone()
-#     cur.close()
-#     conn.close()
-#     # return redirect(url_for('index'))
-#     return render_template('tela_alterar_tag.html', tag=tag)
-
-# @app.route("/remover_tag/<int:id>", methods=['GET'])
-# def remover_tag(id):
-#     conn = sqlite3.connect("database.db")
-#     cur = conn.cursor()
-#     cur.execute("DELETE FROM tags where id = ?", [id])
-#     conn.commit()   
-#     cur.close()
-#     conn.close()
-#     return redirect(url_for('index'))
-
-# @app.route("/adicionar_tags", methods=['POST'])
-# def adicionar_tags():
-#     id = int(request.form.get("id"))
-    
-#     conn = sqlite3.connect("database.db")
-#     cur = conn.cursor()
-
-#     cur.execute("DELETE FROM questoes_tags WHERE questao_id = ?;", [id])
-#     conn.commit()   
-
-#     for tag_id in request.form.getlist('tags'):    
-#         cur.execute("INSERT INTO questoes_tags (questao_id, tag_id) VALUES(?,?);", [id, tag_id])    
-#         conn.commit()   
-
-#     cur.close()
-#     conn.close()
-#     return redirect(url_for('index'))
-
-# @app.route("/alterar_tag", methods=['POST'])
-# def alterar_tag():
-#     conn = sqlite3.connect("database.db")
-#     cur = conn.cursor()
-#     tag = request.form.get("tag")   
-#     id = int(request.form.get("id"))
-#     cur.execute("UPDATE tags SET tag = ? where id = ?", [tag, id])
-#     conn.commit()   
-#     cur.close()
-#     conn.close()
-#     return redirect(url_for('index'))
-
-# @app.route("/alterar_questao", methods=['POST'])
-# def alterar_questao():
-#     conn = sqlite3.connect("database.db")
-#     cur = conn.cursor()
-#     questao = request.form.get("questao")   
-#     id = int(request.form.get("id"))
-#     cur.execute("UPDATE questoes SET questao = ? where id = ?", [questao, id])
-#     conn.commit()   
-#     cur.close()
-#     conn.close()
-#     return redirect(url_for('index'))
-
-# @app.route("/remover_questao/<int:id>", methods=['GET'])
-# def remover_questao(id):
-#     conn = sqlite3.connect("database.db")
-#     cur = conn.cursor()
-#     cur.execute("DELETE FROM questoes where id = ?", [id])
-#     conn.commit()   
-#     cur.close()
-#     conn.close()
-#     return redirect(url_for('listar_questao'))
-
-# @app.route("/adicionar_tag", methods=['POST'])
-# def adicionar_tag():
-#     tag = request.form.get("tag")        
-#     conn = sqlite3.connect("database.db")
-#     cur = conn.cursor()
-#     cur.execute("INSERT INTO tags (tag) VALUES (?)", [tag])
-#     conn.commit()   
-#     cur.close()
-#     conn.close()
-#     return redirect(url_for('index'))
-
-# @app.route("/adicionar_questao", methods=['POST'])
-# def adicionar_questao():
-#     questao = request.form.get("questao")        
-#     conn = sqlite3.connect("database.db")
-#     cur = conn.cursor()
-#     cur.execute("INSERT INTO questoes (questao) VALUES (?)", [questao])
-#     conn.commit()   
-#     cur.close()
-#     conn.close()
-#     return redirect(url_for('index'))
-
-# @app.route("/gerar", methods=['POST'])
-# def gerar():
-#     nro_questao = 1
-#     nro_questao = int(request.form.get("nro_questao"))   
-    
-#     vetTag = list(map(int, request.form.getlist('tags')))       
-    
-#     conn = sqlite3.connect("database.db")
-#     cur = conn.cursor()
-    
-#     if (vetTag is not None):
-#         if (len(vetTag) > 0):
-#             tags = "("
-#             for t in vetTag:
-#                 tags = tags + str(t) + ","
-#             tags = tags + ")"
-#             tags = tags.replace(",)", ")")         
-
-#             cur.execute("SELECT * FROM questoes inner join questoes_tags where tag_id in "+tags+" ORDER BY random() limit "+str(nro_questao))
-#         else:
-#             # print("aqui2")
-#             cur.execute("SELECT * FROM questoes ORDER BY random() limit "+str(nro_questao))
-#     else:
-#         # print("aqui")
-#         cur.execute("SELECT * FROM questoes ORDER BY random() limit "+str(nro_questao))
-    
-#     vetQuestao = cur.fetchall()
-    
-#     cur.close()
-#     conn.close()
-#     return render_template('prova.html', vetQuestao=vetQuestao)
-
-# @app.route("/listar_questao/", defaults={ 'page': 1 })
-# @app.route("/listar_questao/<int:page>")
-# def listar_questao(page):
-#     records = 10
-#     start_from = (page-1)*records
-#     conn = sqlite3.connect("database.db")
-#     cur = conn.cursor()        
-#     cur.execute("select count(*) as total from questoes")    
-#     total_records = cur.fetchone()[0]
-#     total_pages = math.ceil(total_records/records)    
-#     cur.execute("select * FROM questoes order by id desc limit "+str(records)+" offset "+str(start_from))
-#     vet = cur.fetchall()
-#     cur.close()
-#     conn.close()
-#     html_paginacao = ""
-#     i = 1
-#     while (i <= total_pages):        
-#         html_paginacao = html_paginacao + "<a href='"+str(i)+"'>"+str(i)+"</a>&nbsp;&nbsp;"; 
-#         i = i + 1     
-#     return render_template('listar_questao.html', vet=vet, html_paginacao=html_paginacao)
-
-# @app.route("/")
-# def index():
-#     conn = sqlite3.connect("database.db")
-#     cur = conn.cursor()
-#     cur.execute("SELECT * FROM tags")
-#     vetTag = cur.fetchall()
-#     cur.close()
-#     conn.close()
-#     return render_template('index.html', vetTag=vetTag)
-
-# @app.route("/importar")
-# def importar():
-#     # conSqlite3 = sqlite3.connect("database.db")
-#     # curSqlite3 = conSqlite3.cursor()
-#     # curSqlite3.execute("INSERT INTO questoes (questao) VALUES(?)", [questao[1]])
-#     # conSqlite3.commit()     
-    
-#     conn = psycopg2.connect("host=localhost dbname=docente user=postgres password=postgres port=5432")
-#     cur = conn.cursor()
-#     cur.execute("SELECT * FROM avaliacoes where descricao is not null;")
-#     vetQuestao = cur.fetchall()
-#     for questao in vetQuestao:   
-#         # print(questao)            
-#         conSqlite3 = sqlite3.connect("database.db")
-#         curSqlite3 = conSqlite3.cursor()
-#         curSqlite3.execute("INSERT INTO questoes (questao) VALUES(?)", [questao[2]])
-#         conSqlite3.commit()    
-#     cur.close()
-#     conn.close()
-#     return "ok"
